827aad780f04ae60c946f21aad3772ef78b218e2
Breno Poggiali <brenopoggiali@gmail.com>
2019-11-03 21:37:34 -0300

    Add 5th commit

--- AulaPratica-TDD-ES/src/Money.java
+++ AulaPratica-TDD-ES/src/Money.java
@@ -1,23 +1,27 @@
 
-abstract public class Money {
+public class Money {
 	protected int amount;
 	protected String currency;
 
 	public boolean equals(Object object)  {
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
 	}
 	static Money dollar(int amount)  {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 	static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 	Money(int amount, String currency) {
-	  this.amount = amount;
+		this.amount = amount;
 		this.currency = currency;
-  }
+	}
 
-	abstract Money times(int multiplier);
-	abstract String currency();
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+	String currency() {
+		return currency;
+	}
 }
