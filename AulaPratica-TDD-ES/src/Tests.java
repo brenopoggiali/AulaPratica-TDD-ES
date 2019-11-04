aafa854c0c4a90be8e5671f5dd64f493e2bb79b7
Breno Poggiali <brenopoggiali@gmail.com>
2019-11-03 21:03:52 -0300

    Add 4th commit

--- AulaPratica-TDD-ES/src/Tests.java
+++ AulaPratica-TDD-ES/src/Tests.java
@@ -11,7 +11,7 @@ class Tests {
 		assertEquals(Money.dollar(15), five.times(3));
 	}
 
-	@Test	
+	@Test
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
@@ -22,8 +22,14 @@ class Tests {
 
 	@Test
 	public void testFrancMultiplication() {
-	   Money five = Money.franc(5);
-	   assertEquals(Money.franc(10), five.times(2));
-	   assertEquals(Money.franc(15), five.times(3));
+	  Money five = Money.franc(5);
+	  assertEquals(Money.franc(10), five.times(2));
+	  assertEquals(Money.franc(15), five.times(3));
+	}
+
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
 	}
 }
